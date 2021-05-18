0fd5d9a22afc9589dcca3a24dfd65aa1e908628b
GodfreyO96 <76777629+GodfreyO96@users.noreply.github.com>
2021-05-18 09:26:53 +0100

    Add files via upload
    This is a beta version of the code for Javascript (Mixed Message)

---
+++ script.js
@@ -0,0 +1,15 @@
+//All inspirational quotes
+const quote1 = ["Don't let yesterday take up too much of today"];
+const quote2 = ["You learn more from failure than success"];
+const quote3 = ["It's not whether you get knocked down. It's about whether you get up."];
+const quote4 = ["People who are crazy enough to think they can change the world, are the ones who usually do"];
+const quote5 = ["We may encounter many defeats but we must not be defeated"];
+
+//An array of all the arrays above
+const inspirationalQuotes = [quote1, quote2, quote3, quote4, quote5];
+
+//Function:
+let inspirationalQuotesLength = inspirationalQuotes.length;
+for(let i = 0; i < inspirationalQuotesLength; i++) {
+    
+}
